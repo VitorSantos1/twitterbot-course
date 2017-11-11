ef447bd74b007faa666a05a4a8e4477f9a3999eb
Vítor Santos <vitorhgsantos90@gmail.com>
2017-11-11 17:13:48 +0000

    Corporate name generator now posts to Mastodon.

--- post.py
+++ post.py
@@ -2,11 +2,30 @@
 # -*- coding: utf-8 -*-
 
 from utils import post_to_mastodon, post_to_twitter, download_file
+import csv, random
 
-POST_TO_MASTODON = False
+def generate_name():
+    names = []
+    adjectives = []
+
+    with open('nomes.txt', newline='') as namesFile:
+        for row in csv.reader(namesFile):
+            names.append(row[0])
+        
+    with open('adjectivos.txt', newline='') as adjectivesFile:
+        for row in csv.reader(adjectivesFile):
+            adjectives.append(row[0])
+
+    name = random.choice(names)
+    adjective = random.choice(adjectives)
+
+    corporate_name = "{} {}".format(name, adjective)
+    return corporate_name
+
+POST_TO_MASTODON = True
 POST_TO_TWITTER = False
 
-post_content = "Hello world!"
+post_content = generate_name()
 
 print(post_content)
 
